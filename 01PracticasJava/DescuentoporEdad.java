7f1596cd3640ac8ad0efd999d170c5c9a12a8eb9
bviverosa <bviverosa2100@alumno.ipn.mx>
2022-09-16 14:18:39 -0500

    DescuentoporEdad

---
+++ 01PracticasJava/DescuentoporEdad.java
@@ -0,0 +1,14 @@
+/*Name:DescuentoporEdad
+Description: Descuento dependiendo de la Edad del Cliente
+Author:Viveros Alvarado Bryan Dalziel
+*/
+import java.util.scanner;
+class DescuentoporEdad
+        public static void main(String[] args) {
+		system.out.println
+		
+		
+		
+		
+		
+		}
